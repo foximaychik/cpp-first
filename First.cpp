745db67e28c30d08c4e6f64a5148b3b4c2d58a79
foximaychik <128075298+foximaychik@users.noreply.github.com>
2024-02-01 10:01:28 +0400

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    string name = "";
+    cout << "Write your name: \n";
+    cin >> name;
+    cout << "Hello, " + name + "!";
+    return 0;
+}
